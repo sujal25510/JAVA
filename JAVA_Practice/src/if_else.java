01b468402a4594be0885fbe895e92dd4bf3614ae
Sujal Verma <madhvanshiv@gmail.com>
2024-01-22 20:55:20 +0530

    Day2

---
+++ JAVA_Practice/src/if_else.java
@@ -0,0 +1,41 @@
+public class if_else {
+    public static void main(String args[]){
+//        int num1 = 15;
+//        int num2 = 20;
+//        if(num1<10){
+//            System.out.println("A");
+//        }
+//        else if(num1 ==15){
+//            System.out.println("B");
+//        }
+//        else{
+//            System.out.println("C");
+//        }
+
+
+
+
+        //Create two variables length and breadth, and check if it creates a square or not
+        //print "It's square" if true otherwise print "It's not a square"
+
+//        int length=10,breadth=10;
+//
+//        if(length == breadth){
+//            System.out.println("It's a square");
+//        }
+//        else{
+//            System.out.println("It's not a square");
+//        }
+
+        //Write a program to print the absolute values of following numbers
+
+        int a=-10;
+        if(a<0){
+            System.out.println(-1*a);
+        }
+        else{
+            System.out.println(a);
+        }
+
+    }
+}
---
+++ JAVA_Practice/src/operator.java
@@ -0,0 +1,67 @@
+public class operator {
+    public static void main(String[] args){
+        //Arithmatic operator
+//        int b = 5;
+//        int a = 11;
+//        System.out.println("ADD->"+(a+b));
+//        System.out.println("SUB->"+(a-b));
+//        System.out.println("MUL->"+(a*b));
+//        System.out.println("DIV->"+(a/b));
+//        System.out.println("MOD->"+(a%b));
+
+//        int num=5;
+//        num=num+1;
+//        num+=1;
+//        num++;
+//        System.out.println(num);
+
+        //Comparison operators-> used to compare two values/variables
+        //> < >= <= == !=
+//        int a=5;
+//        int b=7;
+//
+//        System.out.println(a==b);
+
+
+
+        //Logical operators
+        // || && !
+
+        int a=5,b=10;
+        int x=3,y=2;
+        System.out.println(a<b && x>y);
+        System.out.println(a<b && x<y);
+        System.out.println(a<b || x>y);
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
