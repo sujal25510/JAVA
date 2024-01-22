c5cf9676dbde44f8bb4be3679ceb9849c1b18f13
Sujal Verma <madhvanshiv@gmail.com>
2024-01-22 18:48:39 +0530

    hello

--- JAVA_Practice/src/start.java
+++ JAVA_Practice/src/start.java
@@ -1,2 +1,9 @@
 public class start {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome to JAVA!");
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
