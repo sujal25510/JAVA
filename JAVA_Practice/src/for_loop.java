c36c081fc25af14610d7ef9e30d2925475cff6a2
Sujal Verma <madhvanshiv@gmail.com>
2024-01-24 19:56:18 +0530

    java

--- JAVA_Practice/src/for_loop.java
+++ JAVA_Practice/src/for_loop.java
@@ -6,8 +6,48 @@ public class for_loop {
 //        }
 
 
-        for(int i=1;i<=10;i++){
-            System.out.println("13 X "+i+" = "+13*i);
-        }
+//        for(int i=1;i<=10;i++){
+//            System.out.println("13 X "+i+" = "+13*i);
+//        }
+
+
+        //NESTED FOR LOOP
+
+
+//        for(int i=0;i<5;i++){
+//            System.out.println("Outer "+i);
+//
+//            for(int j=0;j<4;j++){
+//                System.out.println("Inner "+j);
+//            }
+//        }
+
+//        for(int i=0;i<5;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+
+
+//        for(int i=0;i<4;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.print("\n");
+//        }
+
+
+//        for(int i=5;i>0;i--){
+//            for(int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
     }
 }
+
+
+
