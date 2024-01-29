d6559c5d11706cdb9b7e370b330c135f2f12be4c
Sujal Verma <madhvanshiv@gmail.com>
2024-01-29 20:56:45 +0530

    java

---
+++ JAVA_Practice/src/Array.java
@@ -0,0 +1,71 @@
+public class Array {
+    public static void main(String args[]){
+//        int arr[]={1,2,3,4,5};
+//        System.out.println(arr[3]);
+
+        //int arr[] = new int[10];
+        //String arrString[] = new String[3];
+//        arr[0] = 10;
+//        arr[1] = 20;
+
+//        arrString[0] = "name1";
+//        System.out.println(arr[1]);
+//        System.out.println(arrString[0]);
+
+//        for(int i=0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+
+
+//        String namearr[] = new String[5];
+//        namearr[0] = "Dev";
+//        namearr[1] = "Suryansh";
+//        namearr[2] = "Vedanshu";
+//        namearr[3] = "Satyam";
+//        namearr[4] = "Piyush";
+//
+//        for(int i=0;i<namearr.length;i++){
+//            System.out.println(namearr[i]);
+//        }
+
+
+//        int rollarr[] = new int[5];
+//        rollarr[0] = 34;
+//        rollarr[1] = 7;
+//        rollarr[2] = 31;
+//        rollarr[3] = 57;
+//        rollarr[4] = 46;
+//
+//        for(int i=0;i<rollarr.length;i++){
+//            if(rollarr[i]%2!=0) {
+//                System.out.println(rollarr[i]);
+//            }
+//        }
+
+
+//        float sumarr[] = new float[5];
+//        sumarr[0] = 4;
+//        sumarr[1] = 70;
+//        sumarr[2] = 31;
+//        sumarr[3] = 17;
+//        sumarr[4] = 6;
+//        float sum=0;
+//
+//        for(int i=0;i<sumarr.length;i++){
+//            sum+=sumarr[i];
+//        }
+//        System.out.println(sum);
+
+//        int arr[]={1,2,3,4,5};
+//        for(int i : arr){
+//            System.out.println(i);
+//        }
+
+        int arr[][] = new int[3][2];
+
+    }
+}
+//Fast access -> it is very fast to access any element in an array (by index of the elements), accessing the first element
+//will take same time as accessing the last element.
+
+//Fixed size -> once created, we cannot change the size.
