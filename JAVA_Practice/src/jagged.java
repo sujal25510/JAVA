0775ff63baf6935819de3a78b73c501c73ac7b48
Sujal Verma <madhvanshiv@gmail.com>
2024-01-31 19:53:32 +0530

    jagged

---
+++ JAVA_Practice/src/jagged.java
@@ -0,0 +1,41 @@
+public class jagged {
+//    public static void main(String args[]){
+//        int arr[][] = new int[5][5];
+//        arr[0] = new int[4];
+//        arr[1] = new int[3];
+//        arr[2] = new int[2];
+//
+//        int value = 1;
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<arr[i].length;j++){
+//                arr[i][j] = value;
+//                value++;
+//            }
+//        }
+//
+//
+//
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<arr[i].length;j++){
+//                System.out.println(arr[i][j]+" ");
+//
+//            }
+//        }
+
+
+
+
+    
+//        int arr[][]={{1,2},{3,4,5,6},{7,8,9}};
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<arr[i].length;j++){
+//                System.out.print(arr[i][j]+" ");
+//            }
+//
+//        }
+
+
+
+
+    }
+}
