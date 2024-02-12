6db5c259f68b9f5d26080b7696ad1468f67ec445
Sujal Verma <madhvanshiv@gmail.com>
2024-02-12 20:58:40 +0530

    java

---
+++ JAVA_Practice/src/Constructor.java
@@ -0,0 +1,49 @@
+class Person{
+    public String name;
+    public int age;
+    public int mob;
+
+    public Person(){
+        System.out.println("from constructor");
+        name = "Sujal";
+        age = 11;
+        mob = 1234;
+    }
+
+    public Person(String name,int age,int mob){
+        this.name = name;
+        this.age = age;
+        this.mob = mob;
+    }
+
+    public void output(){
+        System.out.println("Name : "+name+"Age : "+age+"Mob : "+mob);
+    }
+}
+
+
+
+public class Constructor {
+    public static void main(String args[]){
+        Person p1 = new Person("Madhvan",34,2345);
+        Person p2 = new Person();
+//        Person p3 = new Person();
+        System.out.println(p1.name);
+
+    }
+
+
+}
+
+
+
+/*
+-A constructor is a block of code similar to class .
+-It is called when an instance of a class is created.
+-Every time an object is created using the new keyword, at least one constructor is called.
+-Java compiler provides a default constructor by default, this constructor is there if no constructor available in class.
+
+RULES-
+-Constructor name must be same as its class
+-Constructor should have no return type.
+ */
---
+++ JAVA_Practice/src/String.java
@@ -0,0 +1,7 @@
+public class String {
+    public static void main(String args[]){
+        String name = "Hello";
+        String s1 = new String("Hello");
+        System.out.println(s1 + "World");
+    }
+}
